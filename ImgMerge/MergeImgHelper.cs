4d67aa70bda757b528fcafa55ed1a1927625f47d
Thinkbook Plus G5 <Thinkbook Plus G5@LAPTOP-5FRL93QG>
2025-12-26 10:39:35 +0800

    00

--- ImgMerge/MergeImgHelper.cs
+++ ImgMerge/MergeImgHelper.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.IO;
+using System.Runtime.Versioning;
 
 namespace ImgMerge
 {
+    [SupportedOSPlatform("windows")]
     class MergeImgHelper
     {
         /// <summary>
@@ -20,44 +22,88 @@ namespace ImgMerge
         public static Bitmap CombinImage(Image img1, Image img2, int xDeviation = 0, int yDeviation = 0)
         {
             Bitmap bmp = new Bitmap(img1.Width, img1.Height + img2.Height);
-            var p = new Pen(new SolidBrush(Color.LightGray), 1.0f);
-            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
-            p.DashPattern = new float[] { 5, 5 };
-
+            
             using (Graphics g = Graphics.FromImage(bmp))
+            using (var brush = new SolidBrush(Color.LightGray))
+            using (var p = new Pen(brush, 1.0f))
             {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+                p.DashPattern = new float[] { 5, 5 };
+                
                 g.Clear(Color.White);
                 g.DrawImage(img1, 0, 0, img1.Width, img1.Height);
                 g.DrawImage(img2, 0, img1.Height, img2.Width, img2.Height);
                 g.DrawLine(p, 0, img1.Height, img1.Width, img1.Height);
             }
-            GC.Collect();
+            
             return bmp;
         }
 
         public static Bitmap CombinImage(Image[] imgs)
         {
-            Bitmap r = new Bitmap(imgs[0]);
-            for (var i = 1; i < imgs.Length; i++)
+            if (imgs == null || imgs.Length == 0)
             {
-                r = CombinImage(r, imgs[i]);
+                throw new ArgumentException("图片数组不能为空");
             }
-            return r;
+            
+            Bitmap r = new Bitmap(imgs[0]);
+            try
+            {
+                for (var i = 1; i < imgs.Length; i++)
+                {
+                    var temp = CombinImage(r, imgs[i]);
+                    r.Dispose();
+                    r = temp;
+                }
+                return r;
+            }
+            catch
+            {
+                r.Dispose();
+                throw;
+            }
         }
 
         public static Bitmap CombinImage(IEnumerable<string> files)
         {
             List<Image> imgs = new List<Image>();
-            foreach (var f in files)
+            try
             {
-                imgs.Add(Image.FromFile(f));
+                foreach (var f in files)
+                {
+                    imgs.Add(Image.FromFile(f));
+                }
+                
+                if (imgs.Count == 0)
+                {
+                    throw new ArgumentException("没有可合并的图片");
+                }
+
+                Bitmap r = new Bitmap(imgs[0]);
+                try
+                {
+                    for (var i = 1; i < imgs.Count; i++)
+                    {
+                        var temp = CombinImage(r, imgs[i]);
+                        r.Dispose();
+                        r = temp;
+                    }
+                    return r;
+                }
+                catch
+                {
+                    r.Dispose();
+                    throw;
+                }
             }
-            Bitmap r = new Bitmap(imgs[0]);
-            for (var i = 1; i < imgs.Count; i++)
+            finally
             {
-                r = CombinImage(r, imgs[i]);
+                // 释放所有加载的图片资源
+                foreach (var img in imgs)
+                {
+                    img.Dispose();
+                }
             }
-            return r;
         }
     }
 }
--- ImgMerge/Program.cs
+++ ImgMerge/Program.cs
@@ -4,193 +4,302 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
 
 namespace ImgMerge
 {
+    [SupportedOSPlatform("windows")]
     class Program
     {
+        private static readonly string[] SupportedExtensions = { ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp" };
+
         static void Main(string[] args)
         {
+            // 显示欢迎信息和使用提示
+            if (args.Length == 0 || (args.Length == 1 && (args[0] == "--help" || args[0] == "-h" || args[0] == "/?")))
+            {
+                ShowHelp();
+                return;
+            }
+
             if (args.Length != 1 && args.Length != 2)
             {
                 ShowErrorMessage();
                 return;
             }
+
             var outFileName = "result.png";
             var arr = args[0].Split('=');
-            System.Drawing.Imaging.ImageFormat imgFormat = System.Drawing.Imaging.ImageFormat.Png;
-            List<string> files = new List<string>();
-            if (args.Length == 1)
-            {
-                #region 解析输入参数
-                if (arr[0] == "--d")
-                {
-                    var fs = Directory.GetFiles(arr[1]);
-                    foreach (var f in fs)
-                    {
-                        var fi = new FileInfo(f);
-                        var ext = fi.Extension.ToLower();
-                        if (ext == ".png"
-                            || ext == ".jpg"
-                            || ext == ".gif"
-                            || ext == ".bmp"
-                            )
-                        {
-                            files.Add(f);
-                        }
-                    }
-                }
-                else if (arr[0] == "--f")
-                {
-                    var fs = arr[1].Split(';');
-                    foreach(var f in fs)
-                    {
-                        var fi = new FileInfo(f);
-                        if (fi.Exists)
-                        {
-                            files.Add(f);
-                        }
-                    }
-                }
-                else if (arr[0] == "--s")
-                {
-                    var fs = Directory.GetFiles(arr[1].Substring(0, arr[1].LastIndexOf('\\')), arr[1].Substring(arr[1].LastIndexOf('\\') + 1));
-                    //Array.Sort(fs);
-                    foreach (var f in fs)
-                    {
-                        var fi = new FileInfo(f);
-                        if (fi.Exists)
-                        {
-                            files.Add(f);
-                        }
-                    }
-                }
-                else
-                {
-                    ShowErrorMessage();
-                    return;
-                }
-                #endregion
-            }
-            if (args.Length == 2)
-            {
-                #region 解析输入参数
-                if (arr[0] == "--d")
-                {
-                    var fs = Directory.GetFiles(arr[1]);
-                    foreach (var f in fs)
-                    {
-                        var fi = new FileInfo(f);
-                        var ext = fi.Extension.ToLower();
-                        if (ext == ".png"
-                            || ext == ".jpg"
-                            || ext == ".gif"
-                            || ext == ".bmp"
-                            )
-                        {
-                            files.Add(f);
-                        }
-                    }
-                }
-                else if (arr[0] == "--f")
-                {
-                    var fs = arr[1].Split(';');
-                    foreach (var f in fs)
-                    {
-                        var fi = new FileInfo(f);
-                        if (fi.Exists)
-                        {
-                            files.Add(f);
-                        }
-                    }
-                }
-                else if (arr[0] == "--s")
-                {
-                    var fs = Directory.GetFiles(arr[1].Substring(0, arr[1].LastIndexOf('\\')), arr[1].Substring(arr[1].LastIndexOf('\\') + 1));
-                    //Array.Sort(fs);
-                    foreach (var f in fs)
-                    {
-                        var fi = new FileInfo(f);
-                        if (fi.Exists)
-                        {
-                            files.Add(f);
-                        }
-                    }
-                }
-                else
-                {
-                    ShowErrorMessage();
-                    return;
-                }
-                #endregion
-                #region 解析输出参数
-                var arr2 = args[1].Split('=');
-                if (arr2[0] == "--o")
-                {
-                    outFileName = arr2[1];
-                    var fi = new FileInfo(outFileName);
-                    var ext = fi.Extension.ToLower();
-                    if (ext == ".png")
-                    {
-                        imgFormat = System.Drawing.Imaging.ImageFormat.Png;
-                    }
-                    else if (ext == ".jpg")
-                    {
-                        imgFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
-                    }
-                    else if (ext == ".gif")
-                    {
-                        imgFormat = System.Drawing.Imaging.ImageFormat.Gif;
-                    }
-                    else if (ext == ".bmp")
-                    {
-                        imgFormat = System.Drawing.Imaging.ImageFormat.Bmp;
-                    }
-                }
-                else
-                {
-                    ShowErrorMessage();
-                    return;
-                }
-                #endregion
-            }
-            if (files.Count < 2)
+            if (arr.Length != 2)
             {
                 ShowErrorMessage();
                 return;
             }
-            //foreach (var f in files) Console.WriteLine(f);
+
+            ImageFormat imgFormat = ImageFormat.Png;
+            List<string> files = new List<string>();
+
+            // 解析输入参数
+            if (!ParseInputFiles(arr[0], arr[1], ref files))
+            {
+                ShowErrorMessage();
+                return;
+            }
+
+            // 解析输出参数（如果提供）
+            if (args.Length == 2)
+            {
+                var arr2 = args[1].Split('=');
+                if (arr2.Length != 2 || arr2[0] != "--o")
+                {
+                    ShowErrorMessage();
+                    return;
+                }
+                outFileName = arr2[1];
+                var fi = new FileInfo(outFileName);
+                var format = GetImageFormatFromExtension(fi.Extension);
+                if (format == null)
+                {
+                    Console.WriteLine($"错误: 不支持的输出格式 '{fi.Extension}'");
+                    Console.WriteLine($"支持的输出格式: PNG, JPG/JPEG, GIF, BMP, WebP");
+                    return;
+                }
+                imgFormat = format;
+            }
+
+            if (files.Count < 2)
+            {
+                Console.WriteLine("错误: 至少需要2张图片才能进行合并");
+                Console.WriteLine($"当前找到 {files.Count} 张图片");
+                ShowErrorMessage();
+                return;
+            }
+
+            Console.WriteLine($"找到 {files.Count} 张图片，开始合并...");
             try
             {
                 GC.Collect();
-                var bmp = MergeImgHelper.CombinImage(files);
-                var fileInfo = new FileInfo(outFileName);
-                if (!Directory.Exists(fileInfo.DirectoryName))
+                using (var bmp = MergeImgHelper.CombinImage(files))
                 {
-                    Directory.CreateDirectory(fileInfo.DirectoryName);
+                    var fileInfo = new FileInfo(outFileName);
+                    if (!string.IsNullOrEmpty(fileInfo.DirectoryName) && !Directory.Exists(fileInfo.DirectoryName))
+                    {
+                        Directory.CreateDirectory(fileInfo.DirectoryName);
+                    }
+
+                    // 保存图像
+                    SaveImage(bmp, outFileName, imgFormat);
+                    Console.WriteLine($"合并完成！输出文件: {outFileName}");
                 }
-                bmp.Save(outFileName, System.Drawing.Imaging.ImageFormat.Png);
             }
-            catch(ArgumentException ae)
+            catch (ArgumentException ae)
             {
-                Console.WriteLine($"{ae.Message}::Memory error." );
+                Console.WriteLine($"错误: {ae.Message} (内存不足)");
+                return;
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("错误: 内存不足，无法处理如此大的图像文件");
+                return;
+            }
+            catch (FileNotFoundException fe)
+            {
+                Console.WriteLine($"错误: 文件未找到 - {fe.FileName}");
                 return;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"错误: {e.Message}");
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine($"内部错误: {e.InnerException.Message}");
+                }
                 return;
             }
         }
 
+        private static bool ParseInputFiles(string argType, string argValue, ref List<string> files)
+        {
+            if (argType == "--d")
+            {
+                // 从目录读取所有支持的图片
+                if (!Directory.Exists(argValue))
+                {
+                    Console.WriteLine($"错误: 目录不存在 - {argValue}");
+                    return false;
+                }
+                var fs = Directory.GetFiles(argValue);
+                foreach (var f in fs)
+                {
+                    var fi = new FileInfo(f);
+                    var ext = fi.Extension.ToLower();
+                    if (IsSupportedExtension(ext))
+                    {
+                        files.Add(f);
+                    }
+                }
+                return true;
+            }
+            else if (argType == "--f")
+            {
+                // 从文件列表读取（分号分隔）
+                var fs = argValue.Split(';');
+                foreach (var f in fs)
+                {
+                    var trimmed = f.Trim();
+                    if (string.IsNullOrEmpty(trimmed)) continue;
+                    var fi = new FileInfo(trimmed);
+                    if (fi.Exists)
+                    {
+                        files.Add(trimmed);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"警告: 文件不存在，已跳过 - {trimmed}");
+                    }
+                }
+                return true;
+            }
+            else if (argType == "--s")
+            {
+                // 使用通配符匹配文件
+                try
+                {
+                    var lastSlashIndex = argValue.LastIndexOf('\\');
+                    if (lastSlashIndex < 0)
+                    {
+                        Console.WriteLine("错误: 通配符路径格式不正确");
+                        return false;
+                    }
+                    var directory = argValue.Substring(0, lastSlashIndex);
+                    var pattern = argValue.Substring(lastSlashIndex + 1);
+                    if (!Directory.Exists(directory))
+                    {
+                        Console.WriteLine($"错误: 目录不存在 - {directory}");
+                        return false;
+                    }
+                    var fs = Directory.GetFiles(directory, pattern);
+                    foreach (var f in fs)
+                    {
+                        var fi = new FileInfo(f);
+                        if (fi.Exists)
+                        {
+                            files.Add(f);
+                        }
+                    }
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private static bool IsSupportedExtension(string ext)
+        {
+            return Array.Exists(SupportedExtensions, e => e.Equals(ext, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static ImageFormat? GetImageFormatFromExtension(string ext)
+        {
+            ext = ext.ToLower();
+            switch (ext)
+            {
+                case ".png":
+                    return ImageFormat.Png;
+                case ".jpg":
+                case ".jpeg":
+                    return ImageFormat.Jpeg;
+                case ".gif":
+                    return ImageFormat.Gif;
+                case ".bmp":
+                    return ImageFormat.Bmp;
+                case ".webp":
+                    // WebP格式，但由于System.Drawing不直接支持，将使用PNG格式保存
+                    // 实际保存时会特殊处理
+                    return ImageFormat.Png;
+                default:
+                    return null;
+            }
+        }
+
+        private static void SaveImage(Bitmap bmp, string fileName, ImageFormat format)
+        {
+            var ext = Path.GetExtension(fileName).ToLower();
+            
+            // WebP格式特殊处理
+            if (ext == ".webp")
+            {
+                // 由于System.Drawing不直接支持WebP，保存为PNG格式
+                // 用户需要其他工具转换，或者可以添加第三方库支持
+                var pngFileName = Path.ChangeExtension(fileName, ".png");
+                Console.WriteLine($"提示: System.Drawing不支持直接保存WebP格式，已保存为PNG格式: {pngFileName}");
+                Console.WriteLine($"提示: 如需WebP格式，请使用其他工具将PNG转换为WebP");
+                bmp.Save(pngFileName, ImageFormat.Png);
+            }
+            else
+            {
+                bmp.Save(fileName, format);
+            }
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("    图片合并工具 (ImgMerge)");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+            Console.WriteLine("功能说明:");
+            Console.WriteLine("  将多张图片垂直合并为一张图片");
+            Console.WriteLine();
+            Console.WriteLine("支持的输入格式:");
+            Console.WriteLine("  PNG, JPG/JPEG, GIF, BMP, WebP");
+            Console.WriteLine();
+            Console.WriteLine("支持的输出格式:");
+            Console.WriteLine("  PNG, JPG/JPEG, GIF, BMP, WebP (WebP将保存为PNG)");
+            Console.WriteLine();
+            Console.WriteLine("使用方法:");
+            Console.WriteLine();
+            Console.WriteLine("  1. 合并目录中的所有图片:");
+            Console.WriteLine("     ImgMerge.exe --d=目录路径 [--o=输出文件]");
+            Console.WriteLine("     示例: ImgMerge.exe --d=.\\source --o=result.png");
+            Console.WriteLine();
+            Console.WriteLine("  2. 合并指定的多个文件:");
+            Console.WriteLine("     ImgMerge.exe --f=文件1;文件2;文件3 [--o=输出文件]");
+            Console.WriteLine("     示例: ImgMerge.exe --f=.\\img1.jpg;.\\img2.png --o=result.jpg");
+            Console.WriteLine();
+            Console.WriteLine("  3. 使用通配符匹配文件:");
+            Console.WriteLine("     ImgMerge.exe --s=目录\\*.jpg [--o=输出文件]");
+            Console.WriteLine("     示例: ImgMerge.exe --s=.\\source\\*.jpg --o=result.jpg");
+            Console.WriteLine();
+            Console.WriteLine("参数说明:");
+            Console.WriteLine("  --d    : 从指定目录读取所有支持的图片文件");
+            Console.WriteLine("  --f    : 指定要合并的文件列表（用分号分隔）");
+            Console.WriteLine("  --s    : 使用通配符匹配文件");
+            Console.WriteLine("  --o    : 指定输出文件名（可选，默认为result.png）");
+            Console.WriteLine("  --help : 显示此帮助信息");
+            Console.WriteLine();
+            Console.WriteLine("注意事项:");
+            Console.WriteLine("  - 至少需要2张图片才能进行合并");
+            Console.WriteLine("  - 图片将按文件名顺序垂直合并");
+            Console.WriteLine("  - 如果输出目录不存在，将自动创建");
+            Console.WriteLine("  - WebP格式的输出将自动转换为PNG格式");
+            Console.WriteLine();
+        }
+
         private static void ShowErrorMessage()
         {
-            Console.WriteLine(@"imgMerge.exe --d=.\source --o=result.png");
-            Console.WriteLine(@"imgMerge.exe --d=.\source");
-            Console.WriteLine(@"imgMerge.exe --f=.\source\01.jpg;.\source\02.jpg --o=result.jpg");
-            Console.WriteLine(@"imgMerge.exe --f=.\source\01.jpg;.\asource\02.jpg");
-            Console.WriteLine(@"imgMerge.exe --s=.\source\*.jpg --o=result.jpg");
-            Console.WriteLine(@"imgMerge.exe --s=.\source\*.jpg");
+            Console.WriteLine("错误: 参数不正确");
+            Console.WriteLine();
+            Console.WriteLine("快速示例:");
+            Console.WriteLine("  ImgMerge.exe --d=.\\source --o=result.png");
+            Console.WriteLine("  ImgMerge.exe --f=img1.jpg;img2.jpg --o=result.jpg");
+            Console.WriteLine("  ImgMerge.exe --s=.\\source\\*.jpg --o=result.jpg");
+            Console.WriteLine();
+            Console.WriteLine("输入 'ImgMerge.exe --help' 查看详细使用说明");
         }
     }
 }
